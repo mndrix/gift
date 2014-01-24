0d7a4372eb0047d6c40b0c147dd9338c666ad0ff
Michael Hendricks <michael@ndrix.org>
2014-01-24 13:41:28 -0700

    Update pack metadata

--- pack.pl
+++ pack.pl
@@ -1,7 +1,11 @@
 name(gift).
-title('').
+title('Command line tool for creating packs').
+
 version('0.0.1').
 download('http://packs.ndrix.com/gift/gift-0.0.1.tgz').
+
+requires(func).
+
 author('Michael Hendricks','michael@ndrix.org').
 packager('Michael Hendricks','michael@ndrix.org').
 maintainer('Michael Hendricks','michael@ndrix.org').
